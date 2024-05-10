587303f2c54b079d6cf311000061763c7bda6df3
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-05-10 16:46:47 +0530

    added comp

---
+++ components/auth/register-form.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export const RegisterForm = () => {
+    return (
+        <div className="basis-full lg:basis-2/3 h-full w-full flex justify-center items-center">
+                Register form
+        </div>
+    )
+}
