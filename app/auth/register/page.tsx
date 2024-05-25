db2096dec6826905b573ceb5f30628f75e857e8e
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-05-25 17:12:45 +0530

    ui: changes

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,11 +1,9 @@
 import { RegisterForm } from "@/components/auth/register-form";
-import { CarouselBlock } from "@/components/carousel-block";
 import React from "react";
 
 const RegisterPage = () => {
     return (
         <div className="flex h-screen">
-            <CarouselBlock />
             <RegisterForm />
         </div>
         
--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -2,7 +2,7 @@
 
 export const RegisterForm = () => {
     return (
-        <div className="basis-full lg:basis-2/3 h-full w-full flex justify-center items-center">
+        <div>
                 Register form
         </div>
     )
