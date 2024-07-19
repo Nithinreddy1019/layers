115cc35ee8f26e67c132a468ce0d5d2a6645ca67
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-07-19 11:40:10 +0530

    added callbacks, events

--- apps/web/app/auth/_components/login-form.tsx
+++ apps/web/app/auth/_components/login-form.tsx
@@ -20,9 +20,15 @@ import { FormError } from "./form-error";
 import { FormSuccess } from "./form-success";
 import { useState, useTransition } from "react";
 import { LoginAction } from "~/actions/login-action";
+import { useSearchParams } from "next/navigation";
 
 
 export const Loginform = () => {
+
+    const searchParams = useSearchParams();
+    const urlError = searchParams.get("error") === "OAuthAccountNotLinked" ? 
+                        "Email already in use with another account" :
+                        ""
 
     const [error, setError] = useState<string | undefined>("");
     const [success, setSuccess] = useState<string| undefined>("");
@@ -112,7 +118,7 @@ export const Loginform = () => {
                                 )}
                             />
                         </div>
-                        <FormError message={error}/>
+                        <FormError message={error || urlError}/>
                         <FormSuccess message={success}/>
                         <Button 
                             type="submit"
--- apps/web/auth.ts
+++ apps/web/auth.ts
@@ -3,10 +3,36 @@ import authConfig from "./auth.config"
  
 import { PrismaClient } from "@prisma/client"
 import { PrismaAdapter } from "@auth/prisma-adapter"
+import { db } from "@repo/db/db"
  
 const prisma = new PrismaClient()
  
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  events:{
+    async linkAccount({user}) {
+      await db.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date() }
+      })
+    }
+  },
+  callbacks: {
+    async jwt({user, token}){
+      return token
+    },
+    async session({ session, token}) {
+
+      if(token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      return session
+    }
+  },
+  pages: {
+    signIn: "/auth/login",
+    error: "/auth/error"
+  },
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
   ...authConfig,
