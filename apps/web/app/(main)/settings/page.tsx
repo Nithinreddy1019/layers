d7ae729171b723f1e26a1b8ff5cae4aa345e4a6a
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-08-09 11:47:02 +0530

    settings page

--- apps/web/app/(main)/settings/page.tsx
+++ apps/web/app/(main)/settings/page.tsx
@@ -1,9 +1,31 @@
-import { ProfileForm } from "../_components/profile-form";
 
 const SettingsPage = () => {
     return (
-        <div className="h-full border-2 border-secondary border-opacity-5 rounded-lg">
-            <ProfileForm />
+        <div className="min-h-full border-2 border-secondary rounded-lg p-2">
+            <h1 className="text-7xl font-bold">How is it not working?</h1>
+            <h1 className="text-7xl font-bold">How is it not working?</h1>
+
+            <h1 className="text-7xl font-bold">How is it not working?</h1>
+
+            <h1 className="text-7xl font-bold">How is it not working?</h1>
+
+            <h1 className="text-7xl font-bold">How is it not working?</h1>
+            <h1 className="text-7xl font-bold">How is it not working?</h1>
+            <h1 className="text-7xl font-bold">How is it not working?</h1>
+
+            <h1 className="text-7xl font-bold">How is it not working?</h1>
+
+            <h1 className="text-7xl font-bold">How is it not working?</h1>
+
+            <h1 className="text-7xl font-bold">How is it not working?</h1>
+            <h1 className="text-7xl font-bold">How is it not working?</h1>
+            <h1 className="text-7xl font-bold">How is it not working?</h1>
+
+            <h1 className="text-7xl font-bold">How is it not working?</h1>
+
+            <h1 className="text-7xl font-bold">How is it not working?</h1>
+
+            <h1 className="text-7xl font-bold">How is it not working?</h1>
         </div>
     );
 }
