60dc97aac2807db57b987cf2caf27c916f840b01
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-12-02 13:59:13 +0530

    Added prisma to packages

--- apps/web/app/(landing)/page.tsx
+++ apps/web/app/(landing)/page.tsx
@@ -1,12 +1,14 @@
+import { Hero } from "../../components/landing/hero";
 import { Navbar } from "../../components/landing/navbar";
 
 
 
 
-export default function Home() {
+export default async function Home() {
   return (
     <>
       <Navbar />
+      <Hero />
       <div className="w-full text-center py-24">
         <p className="text-3xl">hELOOE</p>
         <p className="text-3xl">hELOOE</p>
---
+++ apps/web/components/landing/hero.tsx
@@ -0,0 +1,12 @@
+
+
+
+export const Hero = () => {
+    return (
+        <section className="py-40">
+            <div className="container">
+                Hell
+            </div>
+        </section>
+    )
+}
