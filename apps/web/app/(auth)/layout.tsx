d1ebab0d640b0f2a1a59219314a5438c789e659a
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-12-03 21:45:15 +0530

    Auth pages init

---
+++ apps/web/app/(auth)/layout.tsx
@@ -0,0 +1,22 @@
+import { ImageSlider } from "../../components/authentication/image-slider";
+
+
+
+const AuthLayout = ({
+    children
+}: {
+    children: React.ReactNode
+}) => {
+    return (
+        <div className="w-full ">
+            <div className="grid lg:grid-cols-2 ">
+                <div>
+                    {children}
+                </div>
+                <ImageSlider />
+            </div>
+        </div>
+    );
+}
+ 
+export default AuthLayout;
---
+++ apps/web/app/(auth)/signin/page.tsx
@@ -0,0 +1,9 @@
+const SigninPage = () => {
+    return (
+        <div>
+            Signin
+        </div>
+    );
+}
+ 
+export default SigninPage;
